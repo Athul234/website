69963117c0ba236af7644b9af229a59abd5fa976
Athul Boben <62099220+Athul420@users.noreply.github.com>
2023-02-18 13:20:40 +0000

    added dyn data and api

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+
+jobs=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'bengaluru',
+    'salary':'1000000'
+  },
+  {
+    'id':2,
+    'title':'Data sumn',
+    'location':'bengaluru',
+    'salary':'1003000'
+  },
+  {
+    'id':3,
+    'title':'some Analyst',
+    'location':'bengaluru',
+    'salary':'2100000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",
+                           job=jobs,comp_name='haha')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug ='True')
